b0c6f47605d12a5daea4a022b7ceae6a5436a309
shaanamin13 <aminshaan2810@gmail.com>
2019-05-03 12:29:59 -0700

    Making some react Components

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-import 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import ExampleWork from "./examplework";
 
-console.log("Loaded react-dom");
-console.log("Webpack works");
+const MyWork = [
+  {
+    title: "Xamarin Cross-Platform",
+    image: {
+      desc: "example screenshot of a project involving code",
+      src: "images/example1.png",
+      comment: "Example 1"
+    }
+  },
+  {
+    title: "Unity and C#",
+    image: {
+      desc: "example screenshot of a project involving cats",
+      src: "images/example3.png",
+      comment: "Example 3"
+    }
+  },
+  {
+    title: "MERN Stack Development",
+    image: {
+      desc: "example screenshot of a project involving chemistry",
+      src: "images/example2.png",
+      comment: "example 2"
+    }
+  }
+];
+
+ReactDOM.render(<ExampleWork work = {MyWork}/>, document.getElementById("example-work"));
